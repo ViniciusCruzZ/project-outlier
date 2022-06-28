5458df5986fcf29086256bc4036288e5da3fa28b
Vinícius Cruz <vvinicruzz@gmail.com>
2022-06-28 16:33:34 -0300

    att

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function logar() {
 
     if (email.value == "garixbot@gmail.com" && senha.value == "Isabella151220@") {
         localStorage.setItem("acesso", true);
-
+        
         window.location.href = "index.html";
     }else {
         alert('Usuário ou senha inválidos!')
