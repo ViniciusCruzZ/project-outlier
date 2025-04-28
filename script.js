981f5311ac060cac5345d8b9c722433308ecb8ac
Vinícius Cruz <58305331+ViniciusCruzZ@users.noreply.github.com>
2025-04-28 14:18:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function logar() {
     var email = document.getElementById('email');
     var senha = document.getElementById('password');
 
-    if (email.value == "garixbot@gmail.com" && senha.value == "Isabella151220@") {
+    if (email.value == "garixbot@gmail.com" && senha.value == "1234567890") {
         localStorage.setItem("acesso", true);
         
         window.location.href = "index.html";
